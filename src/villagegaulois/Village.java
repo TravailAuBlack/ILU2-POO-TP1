8bb34089e761df84f1500e7716fea7d048bb33a5
Yassine Lamrabete <lms3977a@ensfsi.onmicrosoft.com>
2024-10-02 17:29:45 +0200

    Fin TP

--- src/villagegaulois/Village.java
+++ src/villagegaulois/Village.java
@@ -2,17 +2,99 @@ package villagegaulois;
 
 import personnages.Chef;
 import personnages.Gaulois;
+import villagegaulois.Etal;
 
 public class Village {
 	private String nom;
 	private Chef chef;
 	private Gaulois[] villageois;
 	private int nbVillageois = 0;
+	private Marche marche;
 
-	public Village(String nom, int nbVillageoisMaximum) {
+	public Village(String nom, int nbVillageoisMaximum, int nbEtals) {
 		this.nom = nom;
 		villageois = new Gaulois[nbVillageoisMaximum];
+		Marche marche = new Marche(nbEtals);
+		
 	}
+	
+	// classe interne 
+	
+	private static class Marche{
+		private Etal[] etals;
+		private int nbEtals;
+		
+		
+		private Marche(int nbEtalsMax) {
+			this.nbEtals = nbEtalsMax;
+			etals = new Etal[nbEtalsMax];
+			for(int i =0; i<= nbEtalsMax; i++) {
+				etals[i] = new Etal();
+			}
+		}
+		
+		private void utiliserEtal(int indiceEtal, Gaulois vendeur, String produit, int nbProduit) {
+				etals[indiceEtal].occuperEtal(vendeur, produit, nbProduit); 
+			 
+		}
+		
+		private int trouverEtalLibre() {
+			for(int i=0; i<=nbEtals;i++) {
+				if(etals[i].isEtalOccupe())
+					return -1;
+			}
+			return -1;
+		}
+		
+		private Etal[] trouverEtals(String produit) {
+			int nbProdEtal =0;
+			for(Etal etal : etals) {
+				if(etal.isEtalOccupe()) {
+					if(etal.contientProduit(produit)) 
+						nbProdEtal++;
+				}	
+			}
+			Etal[] produitEtal = new Etal[nbProdEtal];
+			for(int i = 0; i<=nbEtals; i++) {
+				if (etals[i].contientProduit(produit))
+					produitEtal[i] = etals[i];
+			}
+			return produitEtal;
+			
+		}
+		
+		
+		private Etal trouverVendeur(Gaulois gaulois) {
+			int nbEtal =0;
+			for(Etal etal : etals) {
+				if(etal.isEtalOccupe()) {
+						nbEtal++;
+				}
+			}
+			Etal[] trouverEtal = new Etal[nbEtal];
+			for(int i = 0; i<=nbEtals; i++) {
+				if(trouverEtal[i].getVendeur().equals(gaulois))
+					return trouverEtal[i];
+			}
+			return null;
+			
+		}
+		
+		
+		private String afficherMarche() {
+			int nbEtalOccupe = 0;
+			int nbEtalLibre = 0;
+			for(int i=0; i<= nbEtals; i++) {
+				if(etals[i].isEtalOccupe())
+					nbEtalOccupe++;
+				nbEtalLibre++;
+			}
+			return "Il reste " + nbEtalLibre + " étals non utilisés dans le marché.\n";
+
+		}
+		
+	}
+	// fin classe interne 
 
 	public String getNom() {
 		return nom;
